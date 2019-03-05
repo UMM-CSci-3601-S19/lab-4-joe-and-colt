00bf254b5e84ba1157018575365bf92892c53a10
deglm006 <deglm006@morris.umn.edu>
2019-03-04 19:36:16 -0600

    Try to create server tests It appears that it is not adding documents to the database.

--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -1,4 +1,122 @@
 package umm3601.todo;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.*;
+import org.bson.codecs.*;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.json.JsonReader;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
 public class TodoControllerSpec {
+  private TodoController todoController;
+  private ObjectId samsId;
+
+  @Before
+  public void clearAndPopulateDB() {
+    MongoClient mongoClient = new MongoClient();
+    MongoDatabase db = mongoClient.getDatabase("test");
+    MongoCollection<Document> todoDocuments = db.getCollection("todo");
+    todoDocuments.drop();
+    List<Document> testTodos = new ArrayList<>();
+    testTodos.add(Document.parse("{\n" +
+      "                    owner: \"Chris\",\n" +
+      "                    status: true,\n" +
+      "                    body: \"fjfiowejfio\",\n" +
+      "                    category: \"homework\"\n" +
+      "                }"));
+    testTodos.add(Document.parse("{\n" +
+      "                    owner: \"Pat\",\n" +
+      "                    status: false,\n" +
+      "                    body: \"gerjiogio\",\n" +
+      "                    category: \"homework\"\n" +
+      "                }"));
+    testTodos.add(Document.parse("{\n" +
+      "                    owner: \"Jamie\",\n" +
+      "                    status: true,\n" +
+      "                    body: \"ehgeiof\",\n" +
+      "                    category: \"software design\"\n" +
+      "                }"));
+
+    samsId = new ObjectId();
+    BasicDBObject sam = new BasicDBObject("_id", samsId);
+    sam = sam.append("owner", "Sam")
+      .append("status", false)
+      .append("body", "rhjiogweuiseo")
+      .append("category", "misc");
+
+    Map<String, String[]> emptyMap = new HashMap<>();
+
+    todoDocuments.insertMany(testTodos);
+    todoDocuments.insertOne(Document.parse(sam.toJson()));
+
+    // It might be important to construct this _after_ the DB is set up
+    // in case there are bits in the constructor that care about the state
+    // of the database.
+    todoController = new TodoController(db);
+  }
+
+  private BsonArray parseJsonArray(String json) {
+    final CodecRegistry codecRegistry
+      = CodecRegistries.fromProviders(Arrays.asList(
+      new ValueCodecProvider(),
+      new BsonValueCodecProvider(),
+      new DocumentCodecProvider()));
+
+    JsonReader reader = new JsonReader(json);
+    BsonArrayCodec arrayReader = new BsonArrayCodec(codecRegistry);
+
+    return arrayReader.decode(reader, DecoderContext.builder().build());
+  }
+
+  private static String getOwner(BsonValue val) {
+    BsonDocument doc = val.asDocument();
+    return ((BsonString) doc.get("owner")).getValue();
+  }
+
+  @Test
+  public void getAllTodos() {
+    Map<String, String[]> emptyMap = new HashMap<>();
+    String jsonResult = todoController.getTodos(emptyMap);
+    BsonArray docs = parseJsonArray(jsonResult);
+    System.out.println(docs.size());
+
+    assertEquals("Should be 4 todos", 4, docs.size());
+    List<String> names = docs
+      .stream()
+      .map(TodoControllerSpec::getOwner)
+      .sorted()
+      .collect(Collectors.toList());
+    List<String> expectedNames = Arrays.asList("Chris", "Jamie", "Pat", "Sam");
+    assertEquals("Names should match", expectedNames, names);
+  }
+
+  @Test
+  public void getHomeworkCategory() {
+    Map<String, String[]> argMap = new HashMap<>();
+    argMap.put("category", new String[]{"homework"});
+    String jsonResult = todoController.getTodos(argMap);
+    BsonArray docs = parseJsonArray(jsonResult);
+
+    assertEquals("Should be 1 todo", 2, docs.size());
+    List<String> names = docs
+      .stream()
+      .map(TodoControllerSpec::getOwner)
+      .sorted()
+      .collect(Collectors.toList());
+    List<String> expectedNames = Arrays.asList("Chris", "Pat");
+    assertEquals("Names should match", expectedNames, names);
+  }
+
+
 }
